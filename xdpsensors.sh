topic-embedded-products/xdpsensors

commit 303d813edfd051acc9ab8010996d09ebf4568400

Set port to 9990 and enabled nginx

xdpsensors.sh
@@ -2,6 +2,7 @@ PIDFILE=/var/run/xdpsensors.pid
 case "$1" in
     start)
         twistd --pidfile $PIDFILE -o -y /var/www/xdpsensors.tac
+        service nginx start
         ;;
     stop)
         if [ -e $PIDFILE ]

xdpsensors.tac
@@ -7,7 +7,7 @@ port = os.environ.get('XDP_PORT')
 if port:
     port = int(port)
 else:
-    port = 80
+    port = 9990
 root = os.environ.get('XDP_ROOT') or '/var/www'
 
 application = service.Application("XDP Webserver")

commit f7dee5545bfc8dc6c6038b424e74f83149af33e3

xdpsensors.sh: Remove invalid attempt at starting ngnix
ngnix arranges its own service startup, no need to hack it here.

Fix error on startup:
  /etc/init.d/rc: /etc/rc5.d/S88xdpsensors.sh: line 17: service: not found

Fixes: 303d813edfd "Set port to 9990 and enabled nginx"

xdpsensors.sh
@@ -2,7 +2,6 @@ PIDFILE=/var/run/xdpsensors.pid
 case "$1" in
     start)
         twistd --pidfile $PIDFILE -o -y /var/www/xdpsensors.tac
-        service nginx start
         ;;
     stop)
         if [ -e $PIDFILE ]