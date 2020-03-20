f7f76825c14d2910d54bc5c69634f8468c5a7c01
Mike Looijmans <mike.looijmans@topic.nl>
2020-03-20 11:09:38 +0100

    Remove old pages and content and add camera placeholder
    Web page goes berserk if "frame.jpg" is missing, so provide a placeholder
    until the camera works.
    
    Remove the old web content and save some space.

--- xdpsensors.sh
+++ xdpsensors.sh
@@ -1,6 +1,7 @@
 PIDFILE=/var/run/xdpsensors.pid
 case "$1" in
     start)
+        cp /var/www/camera.jpg /tmp/frame.jpg
         xdp-dyplo-app -c 2 -k 5 -w 960 -h 540 -f - | \
         gst-launch-1.0 fdsrc fd=0 blocksize=2073600 do-timestamp=true ! \
         rawvideoparse use-sink-caps=false width=960 height=540 format=bgrx framerate=10/1 ! \
