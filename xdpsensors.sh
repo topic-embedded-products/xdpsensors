b5ef22f0e08363576cdcccb7d083ade5846d9b17
Gijs van Esch <gijs.van.esch@topic.nl>
2020-07-09 09:26:22 +0200

    The standard dyploroute for camera 0 is now pointing to ionnod 0 instead of using xdp-dyplo-app

--- xdpsensors.sh
+++ xdpsensors.sh
@@ -5,9 +5,9 @@ case "$1" in
         xdp-dyplo-app -c 2 -k 5 -w 960 -h 540 -f - | \
         gst-launch-1.0 fdsrc fd=0 blocksize=2073600 do-timestamp=true ! \
         rawvideoparse use-sink-caps=false width=960 height=540 format=bgrx framerate=10/1 ! \
-        videoconvert ! jpegenc ! multifilesink location=/tmp/frame.jpg &
+        videoconvert ! jpgenc ! multifilesink location=/tmp/frame.jpg &
         sleep 1
-        xdp-dyplo-app -c 0 &
+        dyploroute 0,0-0,0
         dyploroute 1,0-2,0
         twistd --pidfile $PIDFILE -o -y /var/www/xdpsensors.tac
         ;;
