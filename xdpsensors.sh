4d6be200c98d67335311b47c40d191ae7e5de44d
Gijs van Esch <gijs.van.esch@topic.nl>
2020-07-13 08:57:01 +0200

    Changed error with jpegenc

--- xdpsensors.sh
+++ xdpsensors.sh
@@ -5,7 +5,7 @@ case "$1" in
         xdp-dyplo-app -c 2 -k 5 -w 960 -h 540 -f - | \
         gst-launch-1.0 fdsrc fd=0 blocksize=2073600 do-timestamp=true ! \
         rawvideoparse use-sink-caps=false width=960 height=540 format=bgrx framerate=10/1 ! \
-        videoconvert ! jpgenc ! multifilesink location=/tmp/frame.jpg &
+        videoconvert ! jpegenc ! multifilesink location=/tmp/frame.jpg &
         sleep 1
         dyploroute 0,0-0,0
         dyploroute 1,0-2,0
